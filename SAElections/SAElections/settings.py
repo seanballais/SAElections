40c913ed9b5150764de96e54d486311040b46067
Sean Francis N. Ballais <sfballais123@gmail.com>
2015-05-21 14:50:47 +0800

    Updated settings.py and urls.py to include the static/ folder

--- SAElections/SAElections/settings.py
+++ SAElections/SAElections/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'SAElections.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -100,3 +102,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/'),
+)
--- SAElections/SAElections/urls.py
+++ SAElections/SAElections/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
