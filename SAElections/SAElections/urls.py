bb86388cdfae729bbdde4a23588171cf82037c6b
Sean Francis N. Ballais <sfballais123@gmail.com>
2015-07-14 17:07:12 +0800

    Added a director that will determine what template to show.

--- SAElections/SAElections/urls.py
+++ SAElections/SAElections/urls.py
@@ -22,11 +22,9 @@ admin.autodiscover()
 urlpatterns = [
     url('', include('django.contrib.auth.urls', namespace='auth')),
     url(r'^login/$', 'django.contrib.auth.views.login'),
-    url(r'^$', 'login.views.home', name='home'),
-    url(r'^/$', 'login.views.home', name='home'),
-    url(r'^redirect/$', 'login.views.redirect', name='redirect'),
+    url(r'^$', 'director.views.direct', name='home'), # The voting/thank you page is here
+    url(r'^/$', 'director.views.direct', name='home'), # and also here
     url(r'^authenticate/$', 'login.views.authenticate', name='authenticate'),
     url(r'^save-votes/(?P<votes>.+)/$', 'voting.views.save_votes', name='save-votes'),
-    url(r'^thank-you/$', 'voting.views.thank_you', name='thank-you'),
     url(r'^admin/', include(admin.site.urls)),
 ] + staticfiles_urlpatterns()
